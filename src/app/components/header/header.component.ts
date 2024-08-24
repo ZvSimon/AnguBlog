02efb3a164f8fa6e2016f7f6398c7ad17088999d
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-08-25 00:01:11 +0200

    add login routerLink

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
 import { ButtonModule } from 'primeng/button';
 import { NgStyle } from '@angular/common';
 import { headerData } from '../../data/headerData';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MenubarModule, ButtonModule, NgStyle],
+  imports: [MenubarModule, ButtonModule, NgStyle,RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
@@ -15,4 +16,6 @@ export class HeaderComponent implements OnInit {
   items: MenuItem[] = headerData.items;
 
   ngOnInit() {}
+
+
 }
